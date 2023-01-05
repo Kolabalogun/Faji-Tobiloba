f06708ec4002393902e5a204ea568681285a815a
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2023-01-05 14:53:32 +0100

    Update Hero.js

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -1,44 +1,31 @@
-import React from 'react'
-import Navbar from '../Others/Navbar'
+import React from "react";
+import Navbar from "../Others/Navbar";
 import { Link } from "react-scroll";
 
-
-import Caption from './Caption'
+import Caption from "./Caption";
 
 const Hero = ({ form }) => {
-    return (
-        <div id='home' className="w-full bg-rubik h-screen  bg-cover">
-            < Navbar />
+  return (
+    <div id="home" className="w-full bg-rubik h-screen  bg-cover">
+      <Navbar />
 
-            <Caption form={form} />
+      <Caption form={form} />
 
+      <Link
+        activeClass="active"
+        to="note"
+        spy={true}
+        smooth={true}
+        offset={-80}
+        duration={800}
+        className="absolute cursor-pointer hover:scale-[0.9]  bottom-5 left-[46%] transition-all duration-200 delay-100  mouse"
+      >
+        <span className="mouse-wheel"></span>
+      </Link>
+    </div>
+  );
+};
 
-
-            <Link
-                activeClass="active"
-                to="note"
-
-                spy={true}
-                smooth={true}
-                offset={-80}
-                duration={800}
-                className="absolute cursor-pointer hover:scale-[0.9]  bottom-5 left-[50%] transition-all duration-200 delay-100  mouse"
-
-            >
-                <span className="mouse-wheel"></span>
-            </Link>
-
-
-
-
-
-
-        </div >
-    )
-}
-
-export default Hero
-
-
+export default Hero;
 
 // style={{ backgroundImage: "url('h1.svg')" }}
