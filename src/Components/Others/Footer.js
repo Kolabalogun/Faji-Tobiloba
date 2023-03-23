4b5ce7e328a8a25b94e4cd8c9a643179334737ec
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2023-03-23 20:05:45 +0100

    commit

--- src/Components/Others/Footer.js
+++ src/Components/Others/Footer.js
@@ -6,24 +6,28 @@ const Footer = () => {
       <div className="flex">
         <a
           href="https://facebook.com/Darasimi-Tobiloba-Faji"
+          target={"_blank"}
           className="rounded-[50px] p-2 bg-black mx-3 cursor-pointer relative hover:bg-blue-500"
         >
           <img src="/facebook.svg" alt="facebok" className="h-5 " />
         </a>
         <a
           href="https://instagram.com/thefajitobiloba"
+          target={"_blank"}
           className="rounded-[50px] p-2 bg-black hover:bg-ig  mx-3 cursor-pointer"
         >
           <img src="/instagram.svg" alt="facebok" className="h-5" />
         </a>
         <a
           href="https://twitter.com/TheFajiTobiloba"
+          target={"_blank"}
           className="rounded-[50px] p-2 bg-black mx-3 hover:bg-twitter cursor-pointer"
         >
           <img src="/twitter.svg" alt="facebok" className="h-5" />
         </a>
         <a
           href="https://www.linkedin.com/in/faji-tobiloba-94a0921b2"
+          target={"_blank"}
           className="rounded-[50px] p-2 hover:bg-linked  bg-black mx-3 cursor-pointer"
         >
           <img src="/linkedin.svg" alt="facebok" className="h-4" />
@@ -33,6 +37,7 @@ const Footer = () => {
       <p className="text-black my-8 font-rubik">TheFajiTobiloba</p>
       <a
         href="https://ibrahimkolabalogun.web.app/"
+        target={"_blank"}
         className="text-black mt-8 font-rubik mb-[-60px] text-[13px]"
       >
         Developed by <span className="text-rubik">Ibrahim</span>
