50d5af585aa653a6dee8a23b2c0e429ecbd7ece8
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2023-03-23 20:03:09 +0100

    commit

--- src/Components/Others/Footer.js
+++ src/Components/Others/Footer.js
@@ -4,18 +4,30 @@ const Footer = () => {
   return (
     <div className="flex border-t-[1px] flex-col items-center justify-center bg-white py-[80px]">
       <div className="flex">
-        <div className="rounded-[50px] p-2 bg-black mx-3 cursor-pointer relative hover:bg-blue-500">
+        <a
+          href="https://facebook.com/Darasimi-Tobiloba-Faji"
+          className="rounded-[50px] p-2 bg-black mx-3 cursor-pointer relative hover:bg-blue-500"
+        >
           <img src="/facebook.svg" alt="facebok" className="h-5 " />
-        </div>
-        <div className="rounded-[50px] p-2 bg-black hover:bg-ig  mx-3 cursor-pointer">
+        </a>
+        <a
+          href="https://instagram.com/thefajitobiloba"
+          className="rounded-[50px] p-2 bg-black hover:bg-ig  mx-3 cursor-pointer"
+        >
           <img src="/instagram.svg" alt="facebok" className="h-5" />
-        </div>
-        <div className="rounded-[50px] p-2 bg-black mx-3 hover:bg-twitter cursor-pointer">
+        </a>
+        <a
+          href="https://twitter.com/TheFajiTobiloba"
+          className="rounded-[50px] p-2 bg-black mx-3 hover:bg-twitter cursor-pointer"
+        >
           <img src="/twitter.svg" alt="facebok" className="h-5" />
-        </div>
-        <div className="rounded-[50px] p-2 hover:bg-linked  bg-black mx-3 cursor-pointer">
+        </a>
+        <a
+          href="https://www.linkedin.com/in/faji-tobiloba-94a0921b2"
+          className="rounded-[50px] p-2 hover:bg-linked  bg-black mx-3 cursor-pointer"
+        >
           <img src="/linkedin.svg" alt="facebok" className="h-4" />
-        </div>
+        </a>
       </div>
 
       <p className="text-black my-8 font-rubik">TheFajiTobiloba</p>
